7e37681b69475d613156ebae05cebe833ca88a5a
Pratyush Mishra <pratyush201998@gmail.com>
2020-03-15 18:38:59 +0530

    first commit

---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url('home/', views.home, name='home'),
+    url('register/', views.register, name='register'),
+    url('login/', views.login_user, name='login'),
+    url('signout/', views.signout, name='signout'),
+]
---
+++ accounts/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from . forms import UserCreateForm
+from .models import UserProfile
+from django.views.generic import (
+    ListView, DetailView,
+    CreateView, UpdateView, DeleteView
+)
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.db.models import Q
+
+# Create your views here.
+
+
+def register(request):
+    form = UserCreateForm()
+
+    if request.method == 'POST':
+        form = UserCreateForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = User.objects.get(username=request.POST['username'])
+            profile = UserProfile(user=user)
+            profile.save()
+
+            new_user = authenticate(username=form.cleaned_data['username'],
+                                    password=form.cleaned_data['password1'])
+            login(request, new_user)
+            return redirect('login')
+
+    return render(request, 'accounts/register.html', {
+        's_form': form
+    })
+
+
+def login_user(request):
+    form = AuthenticationForm()
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+
+    return render(request, 'accounts/login.html', {
+        'l_form': form
+    })
+
+
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+
+@login_required(login_url='/login/')
+def home(request):
+    return render(request, 'accounts/home.html')
