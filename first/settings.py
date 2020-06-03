a988492c46bb3930c8c80e008966bc3021cba1e7
Pratyush Mishra <pratyush201998@gmail.com>
2020-06-03 13:45:05 +0530

    done

--- first/settings.py
+++ first/settings.py
@@ -107,8 +107,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'kabirkhanna634@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'practicegithub20@gmail.com'
+EMAIL_HOST_PASSWORD = 'Sealteam6'
 EMAIL_PORT = 587
 
 # Internationalization
